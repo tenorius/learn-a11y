4708fe7b307d4f65b06a4a7b48503e3e92fd08e1
tenorio <rodrigo.o.tenorio@accenture.com>
2019-03-23 14:54:49 -0300

    tabtrapping

--- js/modal.js
+++ js/modal.js
@@ -10,9 +10,38 @@ cancelButton.addEventListener('click', close);
 // https://github.com/jkup/focusable
 
 function open() {
+  
+  var previouslyFocused = document.activeElement;
+  modal.addEventListener('keydown', trap);
+  
   // Show the modal and overlay
   modal.style.display = 'block';
   modalOverlay.style.display = 'block';
+  
+  function trap(event) {
+    var focusableElements = modal.querySelectorAll('a[href], area[href], input:not([disabled]), select:not([disabled]), textarea:not([disabled]), button:not([disabled]), iframe, object, embed, [tabindex="0"], [contenteditable], audio[controls], video[controls], summary');
+    focusableElements = Array.from(focusableElements);
+    
+    var firstItem = focusableElements[0];
+    var lastItem = focusableElements[focusableElements.length - 1];
+    
+    if(event.key === 'Tab') {
+      if(event.shiftKey){
+        if(document.activeElement === firstItem) {
+          event.preventDefault();
+          lastItem.focus();
+        }
+      } else {
+        if(document.activeElement === lastItem) {
+          event.preventDefault();
+          firstItem.focus();
+        }
+      }
+    } else if(event.key === 27){
+      close();
+    }
+    
+  }
 }
 
 function close() {
